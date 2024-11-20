caaec745dd2f228a645c80f8f8b080e9d22f8f08
thirupathi8 <124600966+thirupathi8@users.noreply.github.com>
2024-11-20 22:59:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { Client, GatewayIntentBits, Message } from 'discord.js';
-import 'dotenv/config'
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 //server server
